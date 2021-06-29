c9c750a7563f4e75ba8b646f8ba7a2769e94bf47
Frances Hung <wyfhung@gmail.com>
2021-06-29 10:23:43 -0400

    added and rewrote MI-Wilson helper functions for code efficiency

--- R/Wilson.R
+++ R/Wilson.R
@@ -1,6 +1,28 @@
+
+
+#' Calculate mean of bernoulli variable (p-hats)
+#'
+#' @param mids_obj mids object created by mice package
+#' @param response string name of binary response variable
+#'
+#' @return Qhats: vector of mean response for each imputed dataset
+#' @export
+#'
+#' @examples
+#' imp = mice::mice(mice::nhanes)
+#' Qhats(imp, "hyp")
+Qhats <- function(mids_obj,response) {
+  qhats = lapply(1:mids_obj$m,
+                 function(i) mice::complete(mids_obj,i) %>%
+                   dplyr::select(response) %>%
+                   colMeans()) %>% unlist()
+
+  return(qhats)
+}
+
 #' Calculate Qbar (average response over MICE datasets)
 #'
-#' @param response string name of endogenous variable
+#' @param response string name of binary response variable
 #' @param mids_obj mids object created by mice package
 #'
 #' @return Qbar: the average response over MICEd datasets.
@@ -11,14 +33,27 @@
 #' imp = mice::mice(mice::nhanes)
 #' Qbar(imp, "hyp")
 #'
-Qbar <- function(mids_obj,response) {
-  qbar = lapply(1:mids_obj$m,
-             function(i) mice::complete(mids_obj, i) %>%
-               dplyr::select(response) %>%
-               colMeans()) %>%
-    unlist() %>% mean()
+Qbar <- function(mids_obj, response) {
+  qbar = Qhats(mids_obj, response) %>% mean()
 
   return(qbar)
+}
+
+#' Calculate Uhats (variance for each imputed dataset)
+#'
+#' @param mids_obj mids object created by mice package
+#' @param response string name of binary response variable
+#'
+#' @return Uhats: vector of response variances for each imputed dataset
+#' @export
+#'
+#' @examples
+#' imp = mice::mice(mice::nhanes)
+#' Uhats(imp,"hyp")
+#'
+Uhats <- function(mids_obj, response) {
+  qhats = Qhats(mids_obj, response)
+  return(qhats*(1-qhats)/(mids_obj$data %>% nrow()))
 }
 
 #' Calculate Ubar (average response variance over MICE datasets)
@@ -35,11 +70,7 @@ Qbar <- function(mids_obj,response) {
 #' Ubar(imp, "hyp")
 #'
 Ubar <- function(mids_obj, response) {
-  uhats = lapply(1:mids_obj$m,
-                function(i) mice::complete(mids_obj, i) %>%
-                  dplyr::select(response) %>%
-                  unlist() %>% mean()) %>%
-    unlist() %>% (\(x) x*(1-x)/(mids_obj$data %>% nrow()))(.)
+  uhats = Uhats(mids_obj, response)
 
   return(sum(uhats)/mids_obj$m)
 }
@@ -60,13 +91,9 @@ Ubar <- function(mids_obj, response) {
 #'
 Bm <- function(mids_obj, response) {
   qbar = Qbar(mids_obj, response)
-  bm = lapply(1:mids_obj$m,
-              function(i) ((mice::complete(mids_obj,i) %>%
-                              dplyr::select(response) %>%
-                              unlist() %>% mean())-qbar)^2) %>%
-    unlist() %>% sum()
+  qhats = Qhats(mids_obj, response)
 
-  return(bm/(mids_obj$m-1))
+  return(sum((qhats-qbar)^2)/(mids_obj$m-1))
 }
 
 #' Estimate variance of proportion point estimate \eqn{\bar{Q}_m}
@@ -105,7 +132,14 @@ Rm <- function(mids_obj, response) {
   ubar = Ubar(mids_obj, response)
   bm = Bm(mids_obj, response)
   m = mids_obj$m
-  rm = (1 + 1/m)*bm/ubar
+
+  if(bm==0) {
+    rm = 0
+    message("Bm is 0, imputed and observed values of bin. variable are the same.")
+  }
+  else {
+    rm = (1 + 1/m)*bm/ubar
+  }
 
   return(rm)
 }
@@ -146,9 +180,23 @@ dof <- function(mids_obj, response) {
 #' mi_wilson(imp, "hyp", 0.95)
 #'
 mi_wilson <- function(mids_obj, response, ci_level) {
+
+  #if confidence interval is invalid
+  if(ci_level<=0 | ci_level>= 1) {
+    stop("CI level must be between 0 and 1.")
+  }
+
   qbar = Qbar(mids_obj, response)
-  rm = Rm(mids_obj, response) #TODO: create this
-  df = dof(mids_obj, response)
+  rm = Rm(mids_obj, response)
+
+  #if variable has one value only
+  if(rm==0) {
+    df = Inf
+  }
+  else {
+    df = dof(mids_obj, response)
+  }
+
   t_score = stats::qt(ci_level, df)
   n = mids_obj$data %>% nrow()
 
@@ -175,6 +223,11 @@ mi_wilson <- function(mids_obj, response, ci_level) {
 #' mi_wald(imp, "hyp", 0.95)
 #'
 mi_wald <- function(mids_obj, response, ci_level) {
+
+  if(ci_level<=0 | ci_level>= 1) {
+    stop("CI level must be between 0 and 1.")
+  }
+
   qbar = Qbar(mids_obj, response)
   tm = Tm(mids_obj, response)
   df = dof(mids_obj, response)
